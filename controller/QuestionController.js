ee54ebc5b1c528549141099ae441d6fcf81878f6
Kshitizt2099 <109498859+Kshitizt2099@users.noreply.github.com>
2024-02-28 21:32:33 +0530

    Update QuestionController.js

--- controller/QuestionController.js
+++ controller/QuestionController.js
@@ -43,38 +43,7 @@ module.exports.create = (req,res)=>{
     })
 }
 
-// Delete Question
-// module.exports.delete = (req, res) => {
-//     const questionId = req.params.questionId;
-  
-//     Questions.findById(questionId).populate("options").then(question => {
-//         if (question) {
-         
-         
-//         const hasVotes = question.options.some(option => {
-//         return option.votes > 0});
-//         console.log("gas votes",hasVotes)
-//         if (hasVotes) {
-//         return res.status(400).json({ message: 'Cannot delete question with options having votes' });
-//         }
-//           return Options.deleteMany({ _id: { $in: question.options } }); // Delete associated options
-//         } else {
-//           return res.status(404).json({ message: 'Question Not Found!' });
-//         }
-             
-//       })
-//       .then(() => {
-//         return Questions.deleteOne({ _id: questionId }); // Delete the question
-//       })
-//       .then(() => {
-//         return res.status(200).json({ message: 'Successfully Deleted Question!!' });
-//       })
-//       .catch(err => {
-//         console.log('Error deleting question', err);
-//         return res.status(500).json({ message: 'Error deleting question' });
-//       });
-//   };
-  
+//Deleting the question  
   
 
 
